d0fac211eb3ea11db2746bf386bc989ee9859889
U-THN14B\cheikh <cham75@ymail.com>
2018-07-03 16:38:58 +0200

    categorie

---
+++ dc.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+include('../includes/navbar.php');
+include('../includes/includes.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../index.css">
+	<link rel="stylesheet" href="dc.css">
+	<title>Drone Racing</title>
+	
+</head<body>
+	<img src="../images/logo.png" id="logo" a>
+
+	<div class="conteneurdc ">
+		<div class="gdc ">
+		<div class="grilledc "><p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+	</div>
+		<div class="grilledc ">  <p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+		 </div>
+		<div class="grilledc col"> <p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+	</div>
+	</div>
+	<div class="gdc">
+		<div class="grilledc ">  <p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+	</div>
+		<div class="grilledc "><p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+	</div>
+		<div class="grilledc "> <p id="nomdrn"><b>NOM_DRONE</b></p> <br><p id="prixcaract"> <u> prix</u>xxxx <br> <u>caracteristique</u></p>xxxxxxxxxxxxxxxxxxxxxx  <div class="plusdinfo"><p class="plusdinfotext">plus d'information</p></div>   <div class="caddie"> <img src="../images/caddie.png" class="imgcaddie"> </div>
+	</div>
+	</div>
+
+	</div>
+	
+
+
+
+</body>
+</html>
